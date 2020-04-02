ae2a12ec4428d119010db0d6c2143907a57c4112
NajiaMustafa <60547945+NajiaMustafa@users.noreply.github.com>
2020-04-02 22:36:59 +0400

    Create SimpleServlet.java

---
+++ Login Form/src/SimpleServlet.java
@@ -0,0 +1,55 @@
+
+
+import java.io.IOException;
+
+
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet("/SimpleServlet")
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		if(null != request.getParameter("register_button")) {
+		String user = request.getParameter("username");
+		System.out.println("username entered is "+ user);
+		//check if user exist in model 
+		RequestDispatcher req = request.getRequestDispatcher("register.html");
+		req.include(request, response);
+		//if not send to error
+		}
+		else {
+			response.sendRedirect("login.html");
+		} 
+	}
+
+}
