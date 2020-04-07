42bb4e571bb1c6c45326e2502cc81c59b1ba4600
dhritix1999 <dhritiadyanthaya14@gmail.com>
2020-04-07 04:21:44 +0400

    Update Users_Database.java

--- Course_Tutoring/src/JDBC/Users_Database.java
+++ Course_Tutoring/src/JDBC/Users_Database.java
@@ -1,5 +1,30 @@
 package JDBC;
 
-public class Users_Database {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+public class Users_Database {
+	DBConnection dbCon;
+	ResultSet rs;
+	
+	public Users_Database() {
+		// TODO Auto-generated constructor stub
+		dbCon = new DBConnection();
+	}
+	
+	
+	public String checkCredentials(String email, String password) {
+		//modify to make each class have the check method
+		String student = "SELECT * FROM STUDENT WHERE STUDENT_ID = "+email+" AND STUDENT_PASSWORD = "+password;
+			rs = dbCon.executeStatement(student);
+			if(rs.isBeforeFirst()) {
+				//check if id is in tutor table if true return tutor else student
+			}
+		//check admin
+			String admin;
+			//if type is ..
+
+	//return "none" or null
+	}
+	
 }
