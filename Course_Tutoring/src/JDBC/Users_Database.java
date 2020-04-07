df71f3a31286930ee0526f034640d292bf86927a
dhritix1999 <dhritiadyanthaya14@gmail.com>
2020-04-07 04:23:26 +0400

    Update Users_Database.java

--- Course_Tutoring/src/JDBC/Users_Database.java
+++ Course_Tutoring/src/JDBC/Users_Database.java
@@ -15,16 +15,17 @@ public class Users_Database {
 	
 	public String checkCredentials(String email, String password) {
 		//modify to make each class have the check method
-		String student = "SELECT * FROM STUDENT WHERE STUDENT_ID = "+email+" AND STUDENT_PASSWORD = "+password;
-			rs = dbCon.executeStatement(student);
-			if(rs.isBeforeFirst()) {
-				//check if id is in tutor table if true return tutor else student
-			}
-		//check admin
-			String admin;
-			//if type is ..
-
-	//return "none" or null
+//		String student = "SELECT * FROM STUDENT WHERE STUDENT_ID = "+email+" AND STUDENT_PASSWORD = "+password;
+//			rs = dbCon.executeStatement(student);
+//			if(rs.isBeforeFirst()) {
+//				//check if id is in tutor table if true return tutor else student
+//			}
+//		//check admin
+//			String admin;
+//			//if type is ..
+//			return admin;
+//
+//	//return "none" or null
 	}
 	
 }
