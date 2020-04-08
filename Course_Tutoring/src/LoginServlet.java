8ee0882819ce8e39b6ccf85707f9e40bcba7fac3
NajiaMustafa <60547945+NajiaMustafa@users.noreply.github.com>
2020-04-09 00:46:48 +0400

    hi

--- Course_Tutoring/src/LoginServlet.java
+++ Course_Tutoring/src/LoginServlet.java
@@ -1,31 +1,40 @@
 import java.io.IOException;
 
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.Key;
+import java.util.Date;
 
 @WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet{
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -39,10 +48,15 @@ public class LoginServlet extends HttpServlet{
 		String user = request.getParameter("username");
 		String pswd = request.getParameter("pwd");
 		System.out.println("username entered is "+ user + pswd);
-		if(AccountModel.validateUser(user, pswd)) {
+		if(AccountModel.validateUser(user, pswd)) {//get user type dhriti
+		String token = Jwts.builder().setSubject("user")
+            .claim("roles", "student").setIssuedAt(new Date())
+            .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
+		Cookie hi = new Cookie("accesstoken",token);
+		response.addCookie(hi);
 		RequestDispatcher req = request.getRequestDispatcher("success.jsp");
 		req.include(request, response);
-		}
+		}	
 		else {
 			response.sendRedirect("error.html");
 		} 
